8c5fe9133210b7ccab2fa39c177f3d975d15cc9b
SapheSaber <anqi24.com@gmail.com>
2023-05-26 16:19:26 -0400

    added descriptiones to Steppie and Voad project

--- src/pages/Steppie.js
+++ src/pages/Steppie.js
@@ -1,10 +1,9 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useLocation } from "react-router-dom";
 import Navbar from '../components/navbar/Navbar';
-import './AllProjects.scss'
-import Scroll from './Scroll'
-import SteppieCover from '../images/SteppieCover.png'
-import Svideo from '../video/SteppieFitnessAppDemo.mp4';
+import Footer from '../components/footer/Footer';
+import Svideo from '../video/SteppieFitnessAppDemo.mp4';;
+
 
 
 export default function Steppie() {
@@ -15,64 +14,107 @@ export default function Steppie() {
   useEffect(() => {
     onTop()
   }, [routePath]);
+
   return (
     <div>
       <Navbar />
-      <div  className='main-container' style={{ 
-      backgroundImage: `url(${SteppieCover})`,
-      height: '90vh',
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat:'no-repeat',
-    }}>
-        <div className="main-cover">
-          <Scroll/>
+
+      <div className='main-container'>
+        <h1 className="project-name">Steppie Mobile App UI & Character Design</h1>
+        <div className="summary">
+          <div className="col">
+            <h3>Duration</h3>
+            <p>3 Weeks</p>
+            <p>(Individual Project)</p>
+          </div>
+          <div className="col">
+            <h3>Role</h3>
+            <p>UI Designer</p>
+            <p>Researcher</p>
+            <p>Interviewer</p>
+          </div>
+          <div className="col">
+            <h3>Skill</h3>
+            <p>Hi-fi Prototyping</p>
+            <p>On-site Interview</p>
+            <p>Research</p>
+          </div>
+          <div className="col">
+            <h3>Tool</h3>
+            <p>Figma</p>
+            <p>Adobe Photoshop</p>
+            <p>Adobe Illustrator</p>
+          </div>
+
         </div>
-        <h2 className="project-name">Steppie Mobile App UI & Character Design</h2>
+
+        <hr></hr>
         <div className="body-container">
-          <div className="body-intro">
-            <div className="body-left">
-              <h3 className="left-heading">Overview</h3>
-             
+          <div className="body-left">
+            <h2 className="left-heading">Overview.</h2>
+          </div>
+
+          <div className="body-right">
+            <div className="para">
+              <p>This is the term project for CMU 15-112: Fundamentals of Programming. In this TP, my goal was to rebuild the popular tower-defense game, Plants vs Zombies using Python, the CMU 112 graphics package and the existing PvsZ characters and graphics. The game was originally developed by PopCap Games and published by Electronic Arts. I will make a simplified version of that, which will only include a few types of plants, zombies, and two levels (the day level and the night level). The player's goal is to successfully defeat waves of zombies using the plants that are provided for each level.</p>
             </div>
+          </div>
+        </div>
 
-            <div className="body-right">
-              <h3 >Methods</h3>
-              <div className="block-names">
-                <div className="block">Prototyping</div>
-                <div className="block">Wireframing</div>
-                <div className="block">Site Interview</div>
-                <div className="block">Research</div>
+        <hr></hr>
+        <div className="body-container">
+          <div className="body-left">
+            <h2 className="left-heading">Research.</h2>
+          </div>
 
-              </div>
+          <div className="body-right">
+            <div className="para">
+              <p>"Steps" are a prominent feature of Pittsburgh's geography and culture. Pittsburgh contains many steep hillsides and numerous sets of public staircases that connect different neighborhoods.</p>
 
-              <div className="body-right">
-                <h3 className="title">Tools</h3>
-                <div className="block-names">
-                  <div className="block">Figma</div>
-                  <div className="block">Adobe Photoshop</div>
-                  <div className="block">Adobe Illustrator</div>
-                </div>
+              <p>There are over 700 steps built in the late 19th and early 20 to provide easy access to various amenities like parks, shops, and neighborhoods. Then, the "Steps" became an integral part of Pittsburgh's culture and identity.</p>
 
             </div>
           </div>
-
-
-            </div>
-            <div className="body-context">
-            <h3 className="title">Video Demo</h3>
-            <div  className="video-container">
-            <video src={Svideo} width='520px' controls='true' className='video'></video>
-            </div>
-
-            </div>
-            <div className="body-context">
-            <h3 className="title">Reflection</h3>
-
-
-            </div>
         </div>
+
+        <hr></hr>
+        <div className="body-container">
+          <div className="body-left">
+            <h2 className="left-heading">Video Demo.</h2>
+          </div>
+
+          <div className="body-right">
+            <div className="para">
+              <video src={Svideo} width='520px' controls='true' className='video'></video>
+            </div>
+          </div>
+        </div>
+
+        <hr></hr>
+        <div className="body-container">
+          <div className="body-left">
+            <h2 className="left-heading">Reflection.</h2>
+          </div>
+
+          <div className="body-right">
+            <div className="para">
+              <p>Embarking on my first coding project, I found the practical application for my Python knowledge in the creation of a game. I also noticed the importance of initial planning, including the setup of the file structure and defining character interactions.</p>
+              <br></br>
+              <p>A significant challenge encountered during the implementation phase was the trajectory algorithm for the Kernel-pulp and the Cattail projectiles. This proved complex due to the need to continuously monitor their collision status with the oncoming zombies. Initially, I used the projectile motion formula, but found it inadequate during integration testing. The crux of the problem lay in the delay between the bullet launch and its point of impact on the moving zombie. To overcome this, I found an alternative solution that instead aimed the projectile towards a point several units ahead of the incoming zombie. This adjustment allowed the zombie to advance into the path of the projectile, thus ensuring successful hits. The implementation of this corrective measure markedly improved the effectiveness of the Cattail and Kernel-pult attacks.</p>
+              <br></br>
+              <p>Reflecting on the final product, I am pleased with my first game development. Nevertheless, I recognize the potential improvements for this game. For example, the incorporation of animations for all characters could significantly enrich the interactivity and user experience. This might require storing a series of images for each character in an array and developing an animation function to cycle through them. The timer function would then continually run these animation functions, thereby introducing dynamic elements into the game.</p>
+              <br></br>
+              <p>This project served as a valuable learning journey, providing insights into the practical aspects of coding. It has galvanized my interest in programming and I look forward to building upon these skills in future projects.</p>
+
+              <br></br>
+              <br></br>
+            </div>
+          </div>
+
+        </div>
+
       </div>
+
     </div>
   )
 
--- src/pages/Voad.js
+++ src/pages/Voad.js
@@ -2,9 +2,8 @@ import React, { useEffect } from 'react'
 import { useLocation } from "react-router-dom";
 import Navbar from '../components/navbar/Navbar';
 import './AllProjects.scss';
-import Assets from '../images/PvsZ/Assets.png';
-import Logistic from '../images/PvsZ/logistic.png';
-import Structure from '../images/PvsZ/structure.png';
+import Kai from '../images/Voad/Kai.png';
+import Route from '../images/Voad/route.png';
 
 
 export default function Voad() {
@@ -34,13 +33,15 @@ export default function Voad() {
             <p>Researcher</p>
           </div>
           <div className="col">
+            <h3>Skill</h3>
+            <p>User Flows and Interaction Design</p>
+            <p>Information Architecture</p>
+            <p>Accessibility Design</p>
+          </div>
+          <div className="col">
             <h3>Tool</h3>
             <p>Figma</p>
             <p>Voiceflow</p>
-          </div>
-          <div className="col">
-            <h3>Course</h3>
-            <p>Interaction Design Overview</p>
           </div>
 
         </div>
@@ -53,7 +54,7 @@ export default function Voad() {
 
           <div className="body-right">
             <div className="para">
-            <h3>VOAD: Voice Operated Assistant for Delivery</h3>
+              <h3>VOAD: Voice Operated Assistant for Delivery</h3>
               <br></br>
               <p>The CUI (Conversational User Interface) allows user to control various aspect of the vehicles, monitor its performance and receive real-time information about the trip. Through a series of user research and analysis , we were able to create real life scenarios, anticipate possible situations, and response to user’s need better. With the help of journey map and usability test, we put more consideration into what is probable situation and what is possible situation. After a series of iterations, we created engaging and supportive CUI for our delivery driver Kai. </p>
             </div>
@@ -63,21 +64,47 @@ export default function Voad() {
         <hr></hr>
         <div className="body-container">
           <div className="body-left">
-            <h2 className="left-heading">Research.</h2>
+            <h2 className="left-heading">Persona.</h2>
           </div>
 
           <div className="body-right">
-            <div className="para">              
-              <p>Prior to initiating the coding phase, I developed a comprehensive plan for this game. This plan encapsulated various crucial elements such as potential characters, requisite assets, an effective file structure, and basic logistical considerations.</p>
+            <div className="para">
+              <h3>Kai Ramos</h3>
               <br></br>
-              <h3>Assets</h3>
-              <img src={Assets} alt="Assets" />
+              <img id="kai-img" src={Kai} alt="An image of Kai Ramos" />
+              <p>Profession: Food and Grocery Deliverer</p>
               <br></br>
-              <h3>Logistic</h3>
-              <img src={Logistic} alt="Logistic" />
+              <p>Kai Ramos was laid off at the start of the COVID-19 pandemic and began working for a variety of online food ordering and delivery platforms to make a living. Because this is now Kai's primary form of income, she wants to make the most out of the time she has each day. Kai loves her autonomous SUV named "Sparky" and enjoys being on the road because she can multi-task, read, or watch movies. Scenario At the end of a long day, Kai finds that there is a large catering delivery available. The drive is 45 minutes from the caterer to the delivery address and although tired, Kai realizes this is a great opportunity to make the most of the trip and find other deliveries to make along the way.</p>
               <br></br>
-              <h3>Structure</h3>
-              <img src={Structure} alt="Structure" />
+            </div>
+          </div>
+        </div>
+        <hr></hr>
+
+        <div className="body-container">
+          <div className="body-left">
+            <h2 className="left-heading">Brainstorming.</h2>
+          </div>
+
+          <div className="body-right">
+            <div className="para">
+              <h3>Plotting Route</h3>
+              <br></br>
+              <p>During the brainstorming process, I decided to visualize all the delivery points on an actual map of Pittsburgh. This approach assisted me in hypothesizing various solutions that the user, Kai, might undertake during her 45-minute delivery task.</p>
+              <img src={Route} alt="Screenshot of Google map of Pittsburgh displaying all delivery points" />
+
+              <h3>Milestones</h3>
+              <br></br>
+              <h4>Milestone 01 - Beginning</h4>
+              <p>At the beginning, Kai asks the CUI about the number of deliveries she has today and the estimated time to get through all the delivery points.</p>
+              <br></br>
+              <h4>Milestone 02 - Middle</h4>
+              <p>Due to the Saturday night market, there will be a change in route around the squirrel hill area. The voice assistant will update Kai with the change in route(time, speed, or delivery stop). The voice assistance will also ask Kai the confirmation of the route change. Kai might ask the voice assistance to find the shortest route possible or a second best route than connect all the delivery points. During the drive, the voice assistance will update Kai on the road conditions such as ‘we are entering a construction site’, ‘there is a traffic 3 min ahead’, etc. </p>
+              <br></br>
+              <h4>Milestone 03 - End</h4>
+              <p>After Kai finishes all of her deliveries, the CUI will ask her if she is ready to take a few more deliveries, if so, how many extras is she comfortable taking. CUI will also be checking in on the fuel condition of Kai’s SUV, If Kai is low on gas, the CUI will direct her to the nearest gas station. If Kai would like to go home, CUI will be providing two options: One is to navigate Kai straight to her house, or she could potentially complete some deliveries along the way home. Finally, when Kai arrives home, the voice assistant will give a brief summary on what Kai did for the day (Numbers of deliveries, time she drove and miles the drove etc.).</p>
+              <br></br>
+
             </div>
           </div>
         </div>
@@ -85,7 +112,7 @@ export default function Voad() {
         <hr></hr>
         <div className="body-container">
           <div className="body-left">
-            <h2 className="left-heading">Video Demo.</h2>
+            <h2 className="left-heading">Voice Demo.</h2>
           </div>
 
           <div className="body-right">
@@ -104,9 +131,15 @@ export default function Voad() {
 
           <div className="body-right">
             <div className="para">
-              <p>It took me around two weeks to complete this project. I split the work into three stages: working-stage (general game elements set up complete and the game is playable with some bugs), MVP (Implementing the corn-catapult and ensuring the Day-Time Mode is complete), and Final Stage (Implementing the Nigh mode and all features are working in the game with no bugs).
+              <p>In this experience of designing and implementing a voice-controlled user interface, I came to understand the unique nature of conversational user interfaces (CUI) compared to the visual interfaces I had previously created. Creating a CUI needs careful consideration of potential user interactions and the integration of error handling features for incorrect commands. After reading the article "Customer Journey Mapping," I recognized the journey map's significance in analyzing a specific product, pinpointing pain points, and facilitating improvements. By employing a journey map and defining a persona, I successfully developed a CUI script tailored to address users' needs for certain scenarios.
               </p>
+              <br></br>
+              <p>Additionally, conducting user testing provided me valuable information for improving the CUI. For example, based on user input, incorporating multiple utterances and alternative requests was essential to meet users' diverse needs. This led to the implementation of an "information" path to accommodate users seeking further information on topics (like gas station refill) with the CUI.</p>
+              <br></br>
+              <p>This unique experience has equipped me with valuable skills that I can apply to future voice interface projects, ultimately broadening my expertise in the realm of CUI design and user experience. </p>
+
             </div>
+            <br></br><br></br>
           </div>
 
         </div>
